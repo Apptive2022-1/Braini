4060dc1164ac76b16f6af1e4435c9309e26d22f9
kimsungjin <kimsungjin0907@gmail.com>
2022-04-27 21:36:35 +0900

    feat: 로그인 화면 구성 완료

--- app/src/main/java/com/example/layoutpractice/view/Login.kt
+++ app/src/main/java/com/example/layoutpractice/view/Login.kt
@@ -1,29 +1,89 @@
 package com.example.layoutpractice.view
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.layoutpractice.R
 
 @Composable
 fun LoginScreen() {
     Box(modifier = Modifier.fillMaxSize()) {
-        Text(text = "LinkIt")
-        Button(onClick = { /*TODO*/ }) {
-            Text("구글로 로그인")
-        }
-        Button(onClick = { /*TODO*/ }) {
-            Text("네이버로 로그인")
-        }
-        Button(onClick = { /*TODO*/ }) {
-            Text("카카오로 로그인")
-        }
-        TextButton(onClick = { /*TODO*/ }) {
-            Text("로그인 건너뛰기")
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+
+
+            Spacer(modifier = Modifier.height(50.dp))
+
+
+            Text(text = "Linkit",
+                fontSize = 50.sp
+                )
+
+            Text(text = "로그인")
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Button(
+                modifier = Modifier.width(250.dp),
+                onClick = { /*TODO*/ }) {
+                Image(painter = painterResource(id = R.drawable.random_image_01),
+                      contentDescription = null,
+                      modifier = Modifier
+                          .size(30.dp)
+                          .clip(CircleShape))
+                Spacer(modifier = Modifier.width(15.dp))
+                Text(text="구글 계정으로 로그인")
+            }
+            
+            Spacer(modifier = Modifier.height(10.dp))
+            
+            Button(
+                modifier = Modifier.width(250.dp),
+                onClick = { /*TODO*/ }) {
+                Image(painter = painterResource(id = R.drawable.random_image_02),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(30.dp)
+                        .clip(CircleShape))
+                Spacer(modifier = Modifier.width(15.dp))
+                Text("네이버 계정으로 로그인")
+            }
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Button(
+                modifier = Modifier.width(250.dp),
+                onClick = { /*TODO*/ }) {
+                Image(painter = painterResource(id = R.drawable.random_image_03),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(30.dp)
+                        .clip(CircleShape))
+                Spacer(modifier = Modifier.width(15.dp))
+                Text("카카오 계정으로 로그인")
+            }
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            TextButton(onClick = { /*TODO*/ }) {
+                Text("로그인 건너뛰기>",
+                    fontSize = 10.sp)
+            }
         }
     }
 }

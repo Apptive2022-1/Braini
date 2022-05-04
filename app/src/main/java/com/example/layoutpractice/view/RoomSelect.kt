ffed7588540719f37d629bff0df914e7bb6def92
Won Suk <daily142857@gmail.com>
2022-05-04 19:42:27 +0900

    feat: 방 선택 옵션 화면 기본틀

---
+++ app/src/main/java/com/example/layoutpractice/view/RoomSelect.kt
@@ -0,0 +1,19 @@
+package com.apptive.braini.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.apptive.braini.ui.theme.LayoutPracticeTheme
+
+@Composable
+fun RoomSelectScreen() {
+    
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun RoomSelectScreenPreview() {
+    LayoutPracticeTheme() {
+        
+    }
+    RoomSelectScreen()
+}

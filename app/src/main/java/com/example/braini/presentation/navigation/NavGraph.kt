2e1256c9ca4b6a26aaf9a0a2335570e396fcc44f
kimsungjin <kimsungjin0907@gmail.com>
2022-05-09 20:30:49 +0900

    feat: 네비게이션기능추가

---
+++ app/src/main/java/com/example/braini/presentation/navigation/NavGraph.kt
@@ -0,0 +1,26 @@
+package com.example.braini.presentation.navigation
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.apptive.braini.view.LoginScreen
+import com.example.braini.presentation.view.AnimatedSplashScreen
+
+@Composable
+fun SetupNavGraph(navController: NavHostController){
+    NavHost(
+        navController =navController ,
+        startDestination = Screen.Splash.route){
+        composable(route = Screen.Splash.route){
+            AnimatedSplashScreen(navController)
+        }
+        composable(route = Screen.Login.route){
+            LoginScreen()
+        }
+    }
+}
--- app/src/main/java/com/example/braini/presentation/navigation/Screen.kt
+++ app/src/main/java/com/example/braini/presentation/navigation/Screen.kt
@@ -2,4 +2,5 @@ package com.example.braini.presentation.navigation
 
 sealed class Screen(val route: String) {
     object Splash : Screen("screen_splash")
+    object Login : Screen("screen_login")
 }

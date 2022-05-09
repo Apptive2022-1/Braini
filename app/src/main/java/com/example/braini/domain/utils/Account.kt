1ff40597a7bf4ba818c04804e77801fe918956d0
Won Suk <daily142857@gmail.com>
2022-05-09 20:43:34 +0900

    feat: 범용 함수 파일 정의

---
+++ app/src/main/java/com/example/braini/domain/utils/Account.kt
@@ -0,0 +1,10 @@
+package com.example.braini.domain.utils
+
+import com.example.braini.domain.model.User
+import com.google.android.gms.auth.api.identity.Identity
+
+/** */
+object SignIn {
+    var currentUser = User.GUEST
+
+}

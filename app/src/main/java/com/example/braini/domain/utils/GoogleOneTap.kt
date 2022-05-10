dcb88b93a7ac7a7a54dcbcb50c878db76c72192f
Won Suk <daily142857@gmail.com>
2022-05-10 10:46:09 +0900

    feat: 구글 One Tap 로그인 구현

--- app/src/main/java/com/example/braini/domain/utils/GoogleOneTap.kt
+++ app/src/main/java/com/example/braini/domain/utils/GoogleOneTap.kt
@@ -1,5 +1,98 @@
 package com.example.braini.domain.utils
 
-object GoogleSignIn {
+import android.app.Activity
+import android.content.Context
+import android.content.IntentSender
+import android.content.IntentSender.SendIntentException
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.IntentSenderRequest
+import com.apptive.braini.R
+import com.example.braini.domain.IntentSenderLauncher
+import com.example.braini.domain.log
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.BeginSignInRequest.GoogleIdTokenRequestOptions
+import com.google.android.gms.auth.api.identity.BeginSignInResult
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.tasks.OnSuccessListener
 
+class GoogleOneTap(
+    // OAuth 결과 계정 정보를 받아오는 데에도 활용된다.
+    private val oneTapClient: SignInClient
+) {
+    fun signOut() = oneTapClient.signOut()
+
+    fun onActicityResult(result: ActivityResult) {
+        when (result.resultCode) {
+            Activity.RESULT_OK -> {
+                val credential = oneTapClient.getSignInCredentialFromIntent(result.data)
+                val idToken = credential.googleIdToken
+                val email = credential.id
+                val name = credential.givenName
+                "One Tap Success!".log()
+                "Token: $idToken \nid: $email \n name: $name\n".log()
+            }
+            Activity.RESULT_CANCELED -> {
+                "One Tap Canceled!".log()
+            }
+        }
+    }
+
+    fun beginSignIn(
+        context: Context,
+        launcher: IntentSenderLauncher
+    ) {
+        val request = providesBeginSignIn(context)
+
+        oneTapClient.beginSignIn(request)
+            .addOnSuccessListener(onSuccessListener(launcher))
+            .addOnFailureListener { e ->
+                "No Google Accounts found. Just continue presenting the signed-out UI.".log()
+                e.localizedMessage.log()
+            }
+    }
+
+    /** 로그인 결과 반환할 정보를 설정한다. (ID Token 또는 Password Credential) */
+    private fun providesBeginSignIn(context: Context): BeginSignInRequest {
+        val idTokenOptions = providesIdTokenRequestOptions(context)
+
+        return BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(idTokenOptions)
+            .setAutoSelectEnabled(true)
+            .build()
+    }
+
+    /** IdToken을 받고자 할 경우, 필요한 옵션 및 정보를 설정한다. */
+    private fun providesIdTokenRequestOptions(context: Context): GoogleIdTokenRequestOptions {
+        val clientId = context.getString(R.string.client_id_server)
+
+        return GoogleIdTokenRequestOptions.builder()
+            .setSupported(true)
+            // Your server's client ID, not your Android client ID
+            .setServerClientId(clientId)
+            // Only show accounts previously used to sign in
+            .setFilterByAuthorizedAccounts(false)
+            .build()
+    }
+
+    /** Client 빌드가 성공적으로 완료된 이후 실행될 콜백 함수 */
+    private fun onSuccessListener(launcher: IntentSenderLauncher): OnSuccessListener<BeginSignInResult> {
+        return object: OnSuccessListener<BeginSignInResult> {
+            override fun onSuccess(p0: BeginSignInResult) {
+                val senderRequest = IntentSenderRequest
+                    .Builder(p0.pendingIntent)
+                    .build()
+
+                try {
+                    launcher.launch(senderRequest)
+                } catch (e: SendIntentException) {
+                    "Couldn't start One Tap UI: ${e.localizedMessage}".log()
+                }
+            }
+        }
+    }
 }

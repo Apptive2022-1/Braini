a99380f7d546fa4c4756853288e8d5510ff0a71f
Won Suk <daily142857@gmail.com>
2022-05-17 01:18:59 +0900

    refactor: Side enum 패키지 이동

--- app/src/main/java/com/example/braini/presentation/view/RoomSelect.kt
+++ app/src/main/java/com/example/braini/presentation/view/RoomSelect.kt
@@ -4,9 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material.TextButton
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -15,14 +13,11 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.apptive.braini.ui.theme.Blue500
-import com.apptive.braini.ui.theme.Blue700
 import com.apptive.braini.ui.theme.LayoutPracticeTheme
-import com.example.braini.Side
+import com.example.braini._enums.Side
 
 @Composable
 fun RoomSelectScreen() {

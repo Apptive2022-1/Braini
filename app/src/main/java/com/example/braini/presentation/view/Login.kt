7b9f76b41ecd28051137d199f0f834d3d37065af
Won Suk <daily142857@gmail.com>
2022-05-16 21:16:17 +0900

    refactor: Login 화면 가독성 개선

--- app/src/main/java/com/example/braini/presentation/view/Login.kt
+++ app/src/main/java/com/example/braini/presentation/view/Login.kt
@@ -4,10 +4,10 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Text
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -20,16 +20,48 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.apptive.braini.R
-import com.example.braini.presentation.view.ModalBottomSheet
 import com.example.braini.presentation.viewmodel.LoginViewModel
+import com.example.braini.presentation.viewmodel.interfaces.ILoginViewModel
+import com.example.braini.presentation.viewmodel.mock.LoginViewModelMock
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun LoginScreen(
-    viewModel: LoginViewModel,
+    viewModel: ILoginViewModel = LoginViewModelMock(),
     navController: NavController
 ){
-    ModalBottomSheet()
+    val coroutineScope = rememberCoroutineScope()
+    val sheetState = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
+
+    ModalBottomSheet(sheetState) {
+        LoginBackground {
+            header()
+            Clouds()
+            Description()
+            Buttons(
+                onClick = {
+                    coroutineScope.launch {
+                        sheetState.show()
+                    }
+                }
+            )
+        }
+    }
+}
+
+@Composable
+private fun LoginBackground(
+    content: @Composable ColumnScope.() -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        content = content
+    )
 }
 
 @Composable
@@ -53,7 +85,7 @@ public fun ColumnScope.Clouds(){
         .padding(top = 50.dp)
         .fillMaxWidth(),
     contentAlignment = Alignment.Center){
-        Image(painter = painterResource(id = R.drawable.people),
+        Image(painter = painterResource(id = R.drawable.random_image_01),
             contentDescription = null,
         modifier = Modifier.size(350.dp))
     }
@@ -117,55 +149,68 @@ public fun ColumnScope.Buttons(onClick: () -> Unit){
 }
 
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun modallogin(){
-Box(modifier = Modifier
-    .fillMaxWidth()
-    .height(400.dp)
-    .background(Color.White)){
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .padding(10.dp),
-    horizontalAlignment = Alignment.CenterHorizontally) {
-        Box(modifier = Modifier
-            .width(200.dp)
-            .height(10.dp)
-            .clip(RoundedCornerShape(50))
-            .background(Color.Gray))
-        Spacer(modifier = Modifier.height(20.dp))
-        Text(text = "SNS 계정으로 로그인")
-        Spacer(modifier = Modifier.height(10.dp))
-        Button(modifier = Modifier
-            .width(350.dp)
-            .clip(RoundedCornerShape(40)),
-            colors = ButtonDefaults.buttonColors(Color.White),
-            onClick = { /*TODO*/ }) {
-            Image(modifier = Modifier.size(30.dp),
-                painter = painterResource(id = R.drawable.google),
-                contentDescription = null )
-            Text(text = "Google로 시작하기")
-        }
-        Spacer(modifier = Modifier.height(8.dp))
-        Button(modifier = Modifier
-            .width(350.dp)
-            .clip(RoundedCornerShape(40)),
-            colors = ButtonDefaults.buttonColors(Color(0xfffbe300)),
-            onClick = { /*TODO*/ }) {
-            Image(modifier = Modifier.size(30.dp),
-                painter = painterResource(id = R.drawable.kakao),
-                contentDescription = null)
-            Text(text = "카카오톡으로 시작하기")
-        }
-        Text(modifier = Modifier.padding(top = 10.dp, bottom = 10.dp),
-            text = "또는")
+private fun ModalBottomSheet(
+    sheetState: ModalBottomSheetState,
+    content: @Composable () -> Unit
+){
+    ModalBottomSheetLayout(
+        sheetContent = {
+            Box(modifier = Modifier
+                .fillMaxWidth()
+                .height(400.dp)
+                .background(Color.White)){
+                Column(modifier = Modifier
+                    .fillMaxSize()
+                    .padding(10.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally) {
+                    Box(modifier = Modifier
+                        .width(200.dp)
+                        .height(10.dp)
+                        .clip(RoundedCornerShape(50))
+                        .background(Color.Gray))
+                    Spacer(modifier = Modifier.height(20.dp))
+                    Text(text = "SNS 계정으로 로그인")
+                    Spacer(modifier = Modifier.height(10.dp))
+                    Button(modifier = Modifier
+                        .width(350.dp)
+                        .clip(RoundedCornerShape(40)),
+                        colors = ButtonDefaults.buttonColors(Color.White),
+                        onClick = { /*TODO*/ }) {
+                        Image(modifier = Modifier.size(30.dp),
+                            painter = painterResource(id = R.drawable.google),
+                            contentDescription = null )
+                        Text(text = "Google로 시작하기")
+                    }
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Button(modifier = Modifier
+                        .width(350.dp)
+                        .clip(RoundedCornerShape(40)),
+                        colors = ButtonDefaults.buttonColors(Color(0xfffbe300)),
+                        onClick = { /*TODO*/ }) {
+                        Image(modifier = Modifier.size(30.dp),
+                            painter = painterResource(id = R.drawable.kakao),
+                            contentDescription = null)
+                        Text(text = "카카오톡으로 시작하기")
+                    }
+                    Text(modifier = Modifier.padding(top = 10.dp, bottom = 10.dp),
+                        text = "또는")
 
-    }
-}
+                }
+            }
+        },
+        sheetState = sheetState,
+        sheetBackgroundColor = Color.White,
+        sheetShape = RoundedCornerShape(30.dp),
+        content = content
+    )
 }
 
 
 @Preview
 @Composable
 fun BPreview(){
-    modallogin()
+    val navController = rememberNavController()
+    LoginScreen(navController = navController)
 }

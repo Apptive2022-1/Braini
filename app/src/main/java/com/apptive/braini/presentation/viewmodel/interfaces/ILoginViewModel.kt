f04d480f5a79343fe1041edb64fd4e7de14c1d8f
Won Suk <daily142857@gmail.com>
2022-05-26 02:26:17 +0900

    refactor: 로그인 화면 리팩토링 적용

--- app/src/main/java/com/apptive/braini/presentation/viewmodel/interfaces/ILoginViewModel.kt
+++ app/src/main/java/com/apptive/braini/presentation/viewmodel/interfaces/ILoginViewModel.kt
@@ -2,9 +2,14 @@ package com.apptive.braini.presentation.viewmodel.interfaces
 
 import android.content.Context
 import androidx.activity.result.ActivityResult
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import com.apptive.braini.domain.IntentSenderLauncher
 
 interface ILoginViewModel {
+    val accountId: MutableState<String>
+    val accountPw: MutableState<String>
+
     fun isLoggedIn(): Boolean
     fun loginWithKakao(context: Context, navigate: () -> Unit)
     fun googleResultListener(result: ActivityResult, navigate: () -> Unit = {})
--- app/src/main/java/com/apptive/braini/presentation/viewmodel/mock/LoginViewModelMock.kt
+++ app/src/main/java/com/apptive/braini/presentation/viewmodel/mock/LoginViewModelMock.kt
@@ -2,11 +2,16 @@ package com.apptive.braini.presentation.viewmodel.mock
 
 import android.content.Context
 import androidx.activity.result.ActivityResult
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import com.apptive.braini.domain.IntentSenderLauncher
 import com.apptive.braini.presentation.viewmodel.interfaces.ILoginViewModel
 import kotlin.random.Random
 
 class LoginViewModelMock: ILoginViewModel {
+    override val accountId: MutableState<String> = mutableStateOf("")
+    override val accountPw: MutableState<String> = mutableStateOf("")
+
     override fun isLoggedIn(): Boolean {
         return Random.nextBoolean()
     }

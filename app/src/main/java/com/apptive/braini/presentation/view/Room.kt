4ce6d08ce88f28342ffb127c54e4eaed34ddf35f
kimsungjin <kimsungjin0907@gmail.com>
2022-06-29 16:45:05 +0900

    feat : draggable memo생성

---
+++ app/src/main/java/com/apptive/braini/presentation/view/Room.kt
@@ -0,0 +1,66 @@
+package com.apptive.braini.presentation.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.consumeAllChanges
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.*
+import kotlin.math.roundToInt
+
+@Composable
+fun MemoCreate(){
+    Box(modifier = Modifier.fillMaxSize()) {
+        var offsetX by remember { mutableStateOf(0f) }
+        var offsetY by remember { mutableStateOf(0f) }
+
+        Box(
+            Modifier
+                .offset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) }
+                .background(Color.Cyan)
+                .size(100.dp)
+                .pointerInput(Unit) {
+                    detectDragGestures { change, dragAmount ->
+                        change.consume()
+                        offsetX += dragAmount.x
+                        offsetY += dragAmount.y
+                    }
+                }
+        )
+    }
+}
+
+@Preview
+@Composable
+fun pr_memo(){
+    MemoCreate()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
